6d7d2498e4300ae1441dc5aa2a871a9b901e02bd
Rickard Sahlgren <rsahlgren@gmail.com>
2022-04-04 10:21:51 +0200

    Trying to understand security group ports.

--- main.tf
+++ main.tf
@@ -81,6 +81,13 @@ resource "aws_security_group" "flaskdev_sg" {
   }
 
   egress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
