9c1f7b66e0fee2d1dc47fd81a67c2857b793a25c
Rickard Sahlgren <rsahlgren@gmail.com>
2022-04-04 09:48:51 +0200

    Avoiding Gunicorn

--- userdata.sh
+++ userdata.sh
@@ -35,21 +35,4 @@ git clone https://github.com/SaxProfanityBandit/learning-terraform-for-aws.git
 export FLASK_APP=learning-terraform-for-aws/index.py
 export FLASK_ENV=development
 
-pip install gunicorn
-
-gunicorn -b 0.0.0.0:8000 app:app
-echo "[Unit]
-Description=Gunicorn instance for a simple hello world app
-After=network.target
-[Service]
-User=ec2-user
-Group=www-data
-WorkingDirectory=/home/ec2-user/flaskapp
-ExecStart=/home/ec2-user/flaskapp/venv/bin/gunicorn -b localhost:8000 app:app
-Restart=always
-[Install]
-WantedBy=multi-user.target" >> /etc/systemd/system/helloworld.service
-
-systemctl daemon-reload
-systemctl start helloworld
-systemctl enable helloworld
+flask run --host 0.0.0.0
