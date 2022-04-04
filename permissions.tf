e30861474b0a0cbafa3e51a095039beb65766a7d
Rickard Sahlgren <rsahlgren@gmail.com>
2022-04-04 10:37:00 +0200

    Seperating out IAM into seperate file

---
+++ permissions.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "FlaskDev_Role" {
+  name = "FlaskDev_Role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codedeploy.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2RoleforAWSCodeDeploy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+  role       = aws_iam_role.FlaskDev_Role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+  role       = aws_iam_role.FlaskDev_Role.name
+}
